243fbfb046bf64f27ada02be24067537a6bded47
abhishekyadav4203689-debug <abhishekyadav4203689@gmail.com>
2026-07-27 02:53:13 +0530

    fixed model path

--- machine_learning/app.py
+++ machine_learning/app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Model load karo
-with open('model.pkl', 'rb') as file:
+with open('models/model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 st.title("JeevanSetu - Disease Prediction System")
