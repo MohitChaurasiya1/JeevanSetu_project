7181869dd42f73dc0a94f84aa7fee8bc669abdec
MohitChaurasiya1 <mohitkumar339900@gmail.com>
2026-09-14 22:26:42 +0530

    database setup ->> backend docker(postgresql)

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -2,8 +2,12 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'jeevansetu_db'),
+        'USER': os.getenv('POSTGRES_USER', 'jeevansetu_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'jeevansetu_password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
